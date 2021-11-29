ea61325e3201b991b2c8b4406bacddc4306a4a5a
Bernard Huff <flyhightech3@gmail.com>
2021-11-29 03:46:11 -0600

    Init commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+var toDoList = [
+    "take out the trash",
+    "wash dishes",
+    "clean out garbage file"
+]
+
+for (var i = 0; i <= toDoList.length; i++) {
+    console.log("This is the beginning" + i);
+}
