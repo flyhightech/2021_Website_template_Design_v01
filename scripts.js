0aed906bdcd81b7552e76c1eb319cfe2fbcc3051
Bernard Huff <flyhightech3@gmail.com>
2021-12-04 21:16:24 -0600

    Organized the files for the projects

--- scripts.js
+++
@@ -1,9 +0,0 @@
-var toDoList = [
-    "take out the trash",
-    "wash dishes",
-    "clean out garbage file"
-]
-
-for (var i = 0; i <= toDoList.length; i++) {
-    console.log("This is the beginning" + i);
-}
