743199bb73ecfd4c4f24e56fb76d7f5768ff813c
Bernard Huff <flyhightech3@gmail.com>
2021-12-08 16:30:04 -0600

    Add event listener to button.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,17 @@
-var num = 5; // Is an inline comment 
+// Is an inline comment 
+
+/* This is a multiline comment 
+in javascript. You gotta make sure that you add in the comments so that your future self will know what's going on in the code.
+*/
+
+var firstButton = document.getElementsByTagName("button")[0];
 
 
-/* This is a multiline comment in javascript */
+firstButton.addEventListener("click" , function() {
+    console.log("Hello World!");
+})
+
+
+
+
+
