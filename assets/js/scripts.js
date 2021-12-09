7812a8b4739f9dde3bd9439a078a8e75a490e78e
Bernard Huff <flyhightech3@gmail.com>
2021-12-08 18:43:17 -0600

    deleted useless code

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,12 +3,16 @@
 /* This is a multiline comment 
 in javascript. You gotta make sure that you add in the comments so that your future self will know what's going on in the code.
 */
+var button = document.getElementById("enterBtn");
+var input = document.getElementById("userInput");
+var ul = document.querySelector("ul");
 
-var firstButton = document.getElementsByTagName("button")[0];
+button.addEventListener("click", function() {
 
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
 
-firstButton.addEventListener("click" , function() {
-    console.log("Hello World!");
 })
 
 
