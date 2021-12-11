f4d303660761e01dfb762bb19010b35ccc88f37c
Bernard Huff <flyhightech3@gmail.com>
2021-12-11 15:55:47 -0600

    Delete styles and scripts you didn't use.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,17 +3,7 @@
 /* This is a multiline comment 
 in javascript. You gotta make sure that you add in the comments so that your future self will know what's going on in the code.
 */
-var button = document.getElementById("enterBtn");
-var input = document.getElementById("userInput");
-var ul = document.querySelector("ul");
 
-button.addEventListener("click", function() {
-
-    var li = document.createElement("li");
-    li.appendChild(document.createTextNode(input.value));
-    ul.appendChild(li);
-
-})
 
 
 
